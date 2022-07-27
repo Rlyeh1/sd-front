772136fd8a446fe544b729af96c131a4fc345105
Santiago <andresnavarro3@hotmail.com>
2022-07-28 02:06:26 +1000

    Homepage

---
+++ src/HomePage.tsx
@@ -0,0 +1,79 @@
+import react from "react";
+import { Container, Table, Button, Alert } from "react-bootstrap";
+import Avatar from "./components/avatar/Avatar";
+import UserDescription from "./components/userDescription/UserDescription";
+import TrainingSessionList from "./components/trainingSessionList/TrainingSessionList";
+import TestImage from "./image/test.jpeg";
+import { Row, Col } from "react-bootstrap";
+import Teams from "./components/teams/Teams";
+
+export interface UserListInterface {
+  user: {
+    name: string;
+    email: string;
+    dateOfBirth: string;
+    nationality: null | string;
+    height: number;
+    weight: number;
+    trainingList: {
+      running: string;
+      walking: string;
+    }[];
+  };
+}
+const HomePage = () => {
+  const userList = {
+    user: {
+      name: "Hello World",
+      email: "Hello@hello.com",
+      dateOfBirth: "12/3/2000",
+      nationality: null,
+      height: 100,
+      weight: 120,
+      trainingList: [{ running: "12/12/12", walking: "1/1/12" }],
+    },
+  } as UserListInterface;
+  const teamsList = {
+    team: {
+      teamName: "TeamName here",
+    },
+  };
+  return (
+    <Container fluid>
+      <Row>
+        <h1>Homepage</h1>
+        <hr />
+      </Row>
+      <Row>
+        <Col className="flex-grow-1" sm={6} md={4} lg={3} xl={2}>
+          <div className="w-100 border-end">
+            <Avatar imageLink={TestImage} />
+          </div>
+        </Col>
+        <Col className=" flex-grow-1 " sm={6} md={7} lg={8} xl={7}>
+          <UserDescription userList={userList} />
+        </Col>
+        <Col className="d-flex flex-direction: column flex-grow-1 justify-content-left align-self-end" sm={6} md={4} lg={3} xl={3}>
+          <Button>My statistics</Button>
+        </Col>
+        <hr />
+      </Row>
+      <Row>
+        <Col md={{ span: 6, offset: 2 }}>
+          <h2>Teams</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 6, offset: 2 }}>
+          <Teams teamsList={teamsList} />  
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 6, offset: 2 }}>
+          <TrainingSessionList trainingList={userList.user.trainingList} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default HomePage;
---
+++ src/components/avatar/Avatar.styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+export const Wrapper = styled.div`
+  background: var(--black);
+  padding: 5px;
+  text-align: center;
+  margin-top: 5px;
+`;
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  display: block;
+  border-radius: 10%;
+`;
